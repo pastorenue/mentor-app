d445cc85f81a6638e9e838fdefd497bc5705b39f
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-08-11 15:01:44 +0100

    change the case of the python in settings file

--- mentor_app/settings.py
+++ mentor_app/settings.py
@@ -21,6 +21,9 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 #Update database configuration with $DATABASE_URL
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
+}
 DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
