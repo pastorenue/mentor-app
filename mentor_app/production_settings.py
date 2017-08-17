6b4a0ab530a4d66a90fc22bbdb3c20458ef40ca4
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-08-17 15:50:21 +0100

    Set the settings file for production

--- mentor_app/production_settings.py
+++ mentor_app/production_settings.py
@@ -22,7 +22,7 @@ ADMINS = (
     ('Pastor Emmanuel', 'pastorenuel@gmail.com'),
 )
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY =os.environ.get('SECRET_KEY', '3dkwehi(vjksjhjk-!dhe3427#@@vcs=./kfdhjreudss')
 EMAIL_HOST_PASSWORD = ''
 ALLOWED_HOSTS =  ['thebossoffice.herokuapp.com']
