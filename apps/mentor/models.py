07b187b950dbfafa4002400600c99e09b8dbab6c
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-08-18 14:43:46 +0100

    Customized the admin interface

--- apps/mentor/models.py
+++ apps/mentor/models.py
@@ -4,6 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 # Create your models here.
+class MentorManager(models.Manager):
+	def get_queryset(self):
+		return super(MentorManager, self).get_queryset().filter(account_status='A')
+
+	def trending_mentors(self):
+		pass
 
 class Mentor(models.Model):
 	AVAILABILITY_CHOICES = (
@@ -18,6 +24,11 @@ class Mentor(models.Model):
 		('long-term', 'Long-term Jobs'),
 		('one-off', 'One-off Jobs')
 	)
+	STATUS_CHOICES = (
+		('A', 'Active'),
+		('D', 'Deleted')
+	)
+
 	title = models.CharField(max_length=10, choices=settings.TITLE_CHOICES)
 	name = models.CharField(max_length=50)
 	user = models.OneToOneField(User)
@@ -32,8 +43,10 @@ class Mentor(models.Model):
 	cv_file = models.FileField("Attach PDF copy of CV ", upload_to='uploads/%Y/%m/%d')
 	slug = models.SlugField(unique=True)
 	linkedin_url = models.URLField("Link to LinkedIn Bio/profile", blank=True)
+	account_status = models.CharField(max_length=1, default='A', choices=STATUS_CHOICES)
 	date_created = models.DateTimeField(auto_now_add=True)
 	date_modified = models.DateTimeField(auto_now=True)
+	objects = MentorManager()
 
 	def __str__(self):
 		return "%s %s" % (self.title, self.name)
--- apps/mentor/views.py
+++ apps/mentor/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Mentor
+from django.views.generic import ListView
 
 # Create your views here.
+class MentorListView(ListView):
+	model = Mentor
+	template_name = 'mentor/mentor_list.html'
+
+	def get_queryset(self):
+		queryset = super(MentorListView, self).get_queryset(**kwargs)
+		params = self.request.GET
+		industry = params.get('industry')
+		if industry != 'all':
+			queryset = Mentor.objects.filter(industry=industry)
+		return queryset
+
+	def get_context_data(self, **kwargs):
+		context = super	(MentorListView, self).get_context_data(**kwargs)
+		context['trending_mentors'] = Mentor.objects.trending_mentors()
+		return context
+
 
 def mentors(request):
 	return render(request, 'mentor/mentor_list.html', {})
