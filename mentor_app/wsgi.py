ccbddbf6036d725ed3ca2e32a5ecf66ff7933ae0
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-08-29 16:01:15 +0100

    modified main.js file

--- mentor_app/wsgi.py
+++ mentor_app/wsgi.py
@@ -19,8 +19,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mentor_app.production_settings"
 
 
 # application = get_wsgi_application()
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
