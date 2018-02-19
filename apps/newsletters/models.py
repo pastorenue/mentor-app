f1cbabad5e20e9a166e26c0c2eb8ca1600fe88d8
GRADE-X <GRADE-X>
2018-02-19 09:19:00 +0100

    created the newsletter subscription model

--- apps/newsletters/models.py
+++ apps/newsletters/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
-class Newsletter(models.Model):
+
+LETTER_CHOICES = (
+	('forbes', 'Forbes'),
+	('business', 'Business')
+)
+class Subscription(models.Model):
 	email = models.EmailField()
 	name = models.CharField(max_length=50, null=True)
 	name_of_company = models.CharField(max_length=100, null=True)
-	sector = model.CharField(max_length=100, null=True)
+	sector = models.CharField(max_length=100, null=True)
 	letter_type = models.CharField(max_length=100, choices=LETTER_CHOICES, null=True)
 
 	def __str__(self):
 		return self.email
+
+class Newsletter(models.Model):
+	letter_type = models.CharField(max_length=100, choices=LETTER_CHOICES, null=True)
+	content = models.TextField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	date_modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "%s --> %s".format(self.letter_type, self.content[:20])
+
+	class Meta:
+		ordering = ('-date_created',)
--- mentor_app/settings.py
+++ mentor_app/settings.py
@@ -60,7 +60,8 @@ INSTALLED_APPS = [
     'anymail',
     'notifications',
     'newsroom',
-    'tinymce'
+    'tinymce',
+    'newsletters'
 ]
 
 MIDDLEWARE = [
